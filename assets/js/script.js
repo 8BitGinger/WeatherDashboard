6e98d931c88cee1a583b113293a5522f9da71fef
8BitGinger <ryan.fann>
2023-09-26 18:36:23 -0500

    finished UI and started basic variables in JS

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var searchEl = document.querySelector('#searchBox');
+var searchBtn = document.querySelector('#searchBtn');
+
+
+//you can make the main box display the last city 
+//searched with local storage if you have time
