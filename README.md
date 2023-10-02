9bd42bc334bdde615e80c0cda154772a191a8e82
8BitGinger <ryan.fann>
2023-10-02 10:34:16 -0500

    updated weather card styling, added event listener for button city searches, added corrected farenheight vs kelvin, updated for live push

--- README.md
+++ README.md
@@ -1,5 +1,39 @@
-Links:
-Tutorial Youtube:
+# Weather Dashboard
+
+Weather Dashboard is designed to search for a specific city and return weather data from a 3rd party API.  In this case openweatherMap API.  
+
+## Visuals
+
+(![Alt text](assets/images/Weather-Live-Screenshot.png))
+
+## YouTube Tutorials and References
+```
 https://www.youtube.com/watch?v=SeXg3AX82ig&t=212s
+
 https://www.youtube.com/watch?v=w0VEOghdMpQ&t=1425s
+
 https://www.youtube.com/watch?v=MIYQR-Ybrn4&t=911s
+```
+
+## Links
+```
+Live URL:  https://8bitginger.github.io/WeatherDashboard
+Repo URL: https://github.com/8BitGinger/WeatherDashboard
+Student Portfolio URL: https://github.com/8BitGinger/studentPortfolio
+```
+
+## Roadmap
+Will need to get the date to show in a more traditional format.  
+
+## Support
+reach out to ryan.fann@gmail.com for questions or support issues
+
+
+## Contributions
+Weather API:
+```
+https://openweathermap.org/api
+```
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,18 @@ const searchButton = document.querySelector(".search-btn");
 const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
+const cityBtnEl = document.querySelector(".city-btn")
 const API_KEY = "5eb79fc09e1638a0fb6c8df9283a6d51"; // API key for OpenWeatherMap API
+
+// opted not to go for JS due to the compelexity of the tutorial, but
+//upate with dayJS for easier date formatting.  
+
+
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card
         return `<div class="details">
-                    <h2>${cityName} <span class="reverse">(${weatherItem.dt_txt.split(" ")[0]})</span></h2>
-                    <h6>Temperature: ${(weatherItem.main.temp - 219.57).toFixed(2)}°F</h6>
+                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                    <h6>Temperature: ${(weatherItem.main.temp).toFixed(2)}°F</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
                 </div>
@@ -18,16 +24,16 @@ const createWeatherCard = (cityName, weatherItem, index) => {
                 </div>`;
     } else { // HTML for the other five day forecast card
         return `<li class="card">
-                    <h3><span class="reverse">(${weatherItem.dt_txt.split(" ")[0]})</span></h3>
+                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-                    <h6>Temp: ${(weatherItem.main.temp - 219.57).toFixed(2)}°F</h6>
+                    <h6>Temp: ${(weatherItem.main.temp).toFixed(2)}°F</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
                 </li>`;
     }
 }
 const getWeatherDetails = (cityName, latitude, longitude) => {
-    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=imperial`;
     fetch(WEATHER_API_URL).then(response => response.json()).then(data => {
         // Filter the forecasts to get only one forecast per day
         const uniqueForecastDays = [];
@@ -89,6 +95,22 @@ const getUserCoordinates = () => {
             }
         });
 }
+
+const getButtonCoordinates = () => {
+    const cityName = cityBtnEl.id;
+    const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+    
+    // Get entered city coordinates (latitude, longitude, and name) from the API response
+    fetch(API_URL).then(response => response.json()).then(data => {
+        if (!data.length) return alert(`No data found for ${cityName}`);
+        const { lat, lon, name } = data[0];
+        getWeatherDetails(name, lat, lon);
+    }).catch(() => {
+        alert("An error occurred while fetching the city!");
+    });
+}
+
+cityBtnEl.addEventListener("click", getButtonCoordinates);
 locationButton.addEventListener("click", getUserCoordinates);
 searchButton.addEventListener("click", getCityCoordinates);
 cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates());
